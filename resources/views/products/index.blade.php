1338ccebc5a7aabce6f5353da938f3d0c1d35ad4
Rafly Ramadhan <Raflyraflyramadhan7477@gmail.com>
2024-03-22 16:48:55 +0700

    styling update

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Branch') }}
+                {{ __('Product') }}
             </h2>
             <a href="{{ route('products.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add Product</a>
         </div>
